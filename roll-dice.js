39addbb670c6e69e1c03c8df5d42b422d2e3716a
Suguna <sugunaalt@gmail.com>
2022-09-08 16:49:30 +0530

    next update

--- roll-dice.js
+++ roll-dice.js
@@ -1,11 +1,12 @@
 var btn = document.getElementById('btn');
-var cube1=document.getElementById('çube1');
-var cube2=document.getElementById('çube2');
+var cube1=document.getElementById('cube1');
+var cube2=document.getElementById('cube2');
 
 var min = 1;
 var max = 24;
 
 btn.onclick = function() {
+  console.log(1);
   var xRand = getRandom(max, min);
   var yRand = getRandom(max, min);
   cube1.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
@@ -13,5 +14,7 @@ btn.onclick = function() {
 }
 
 function getRandom(max, min) {
-  return (Math.floor(Math.random() * (max-min)) + min) * 90;
+  var rotate_deg=(Math.floor(Math.random() * (max-min)) + min) * 90;
+  console.log(rotate_deg);
+  return rotate_deg;
 }
