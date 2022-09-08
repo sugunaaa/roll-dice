13e15de6fe6ec6f456636beb99d6895a754920ba
Suguna <sugunaalt@gmail.com>
2022-09-08 16:19:26 +0530

    back-end start

--- roll-dice.js
+++ roll-dice.js
@@ -1,4 +1,4 @@
-var cube = document.getElementById('cube');
+var cube = document.getElementById('btn');
 
 var min = 1;
 var max = 24;
@@ -6,8 +6,6 @@ var max = 24;
 cube.onclick = function() {
   var xRand = getRandom(max, min);
   var yRand = getRandom(max, min);
-    
-  cube.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
   cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
 }
 
