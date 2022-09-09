ebc7f42bbb4726550d7a1c90bf4f96f2010ddd4f
Suguna <sugunaalt@gmail.com>
2022-09-09 11:32:01 +0530

    dice rotate!!

--- roll-dice.js
+++ roll-dice.js
@@ -1,37 +1,22 @@
-// var btn = document.getElementById('cube1');
-// // var cube1=document.getElementById('cube1');
-// // var cube2=document.getElementById('cube2');
-
-// var min = 1;
-// var max = 24;
-
-// cube1.onclick=function() {
-//         console.log(1);
-//         var xRand = getRandom(max, min);
-//         var yRand = getRandom(max, min);
-//         cube1.style.webkitTransform = `rotateX(${xRand}deg) rotateY(${yRand}deg)`;
-//         cube1.style.transform = `rotateX(${xRand}deg) rotateY(${yRand}deg)`;
-//         // cube2.style.transform = `rotateX(${xRand}deg) rotateY(${yRand}deg)`;
-//     }
-
-// function getRandom(max, min) {
-//   var rotate_deg=(Math.floor(Math.random() * (max-min)) + min) * 90;
-//   console.log(rotate_deg);
-//   return rotate_deg;
-// }
-var cube = document.getElementById('cube');
+var btn = document.getElementById('btn');
+var cube1=document.getElementById('cube1');
+var cube2=document.getElementById('cube2');
 
 var min = 1;
 var max = 24;
 
-cube.onclick = function() {
-  var xRand = getRandom(max, min);
-  var yRand = getRandom(max, min);
-    
-  cube.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
-  cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
-}
+btn.onclick=function() {
+        console.log(1);
+        var xRand1 = getRandom(max, min);
+        var yRand1 = getRandom(max, min);
+        var xRand2 = getRandom(max, min);
+        var yRand2 = getRandom(max, min);
+        cube1.style.transform = `rotateX(${xRand1}deg) rotateY(${yRand1}deg)`;
+        cube2.style.transform = `rotateX(${xRand2}deg) rotateY(${yRand2}deg)`;
+    }
 
 function getRandom(max, min) {
-  return (Math.floor(Math.random() * (max-min)) + min) * 90;
+  var rotate_deg=(Math.floor(Math.random() * (max-min)) + min) * 90;
+  console.log(rotate_deg);
+  return rotate_deg;
 }
